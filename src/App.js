3fc1ce7a5d9a7c3deda191677e398f0dc8556201
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2023-07-04 11:08:05 +0500

    main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
-
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
-
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
